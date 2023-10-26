5859caaf6530efc586f6387840eb6da7c68180a3
Jaroslaw Majcherczyk <jaroslaw.majcherczyk@gmail.com>
2023-10-26 12:01:15 +0200

    first commit of cars showcase

---
+++ components/Hero.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomButton } from "@components";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+        Znajdź, zarezerwuj, wypożycz samochód — szybko i bardzo łatwo!
+        </h1>
+
+        <p className="hero__subtitle">
+          Usprawnij korzystanie z wynajmu samochodu dzięki naszej łatwej rezerwacji
+          proces.
+        </p>
+
+        <CustomButton
+          title="Przejdz do Aut"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
